59fa2b0d5968851d45607feb0392b1bdb18d628e
Gorelov Ruslan <lighttemper@gmail.com>
2019-10-18 02:06:41 +0700

    day7 part1 complete

--- src/day_7.rs
+++ src/day_7.rs
@@ -4,111 +4,86 @@ use std::io::{self, prelude::*, BufReader};
 use std::collections::HashMap;
 // use std::str::FromStr;
 
-//FIXME все значения должны хранить ссылки на другие переменные
-fn smt1(line: &str, map: &mut HashMap<String, u16>) {
-	let re = Regex::new(r"(\w+|\d+)\s->\s(\w+)").unwrap();
+fn get_wire_signal(wire: &str, map: &mut HashMap<String, String>) -> u16 {
+	let re1 = Regex::new(r"^(\w+|\d+)$").unwrap();
+	let re2 = Regex::new(r"(\w+|\d+)\s(AND|OR|LSHIFT|RSHIFT)\s(\w+|\d+)").unwrap();
+	let re3 = Regex::new(r"NOT\s(\w+|\d+)").unwrap();
 
-	let caps = re.captures(line).unwrap();
-	let key = caps.get(2).unwrap().as_str();
-	let operand = caps.get(1).unwrap().as_str();
+	let value = String::from(map.get(wire).unwrap());
+	// println!("{:?} => {:?}", wire, value);
+	if re1.is_match(&value) {
+		let value = match value.parse::<u16>() {
+			Err(_) => get_wire_signal(&value, map),//recursion
+			Ok(r) => r,
+		};
+		map.entry(String::from(wire)).and_modify(|e| *e = value.to_string());
 
+		return value;
+	} else if let Some(caps) = re3.captures(&value) {
+		let operand = caps.get(1).unwrap().as_str();
+		let operand_value = match operand.parse::<u16>() {
+			Err(_) => get_wire_signal(operand, map),//recursion
+			Ok(r) => r,
+		};
 
-	let value = if let Err(_) = operand.parse::<u16>() {
-		*map.entry(operand.to_string()).or_insert(0)
-	} else if let Ok(r) = operand.parse::<u16>() {
-		r
-	} else {
-		0
-	};
-	map.entry(key.to_string()).or_insert(value);
-}
+		let value = !operand_value;
+		map.entry(String::from(wire)).and_modify(|e| *e = value.to_string());
 
-fn smt2(line: &str, map: &mut HashMap<String, u16>) {
-	let re1 = Regex::new(r"(\w+|\d+)\s(AND|OR|LSHIFT|RSHIFT)\s(\w+|\d+)\s->\s(\w+)").unwrap();
-	let re2 = Regex::new(r"NOT\s(\w+|\d+)\s->\s(\w+)").unwrap();
+		return value;
+	} else if let Some(caps) = re2.captures(&value) {
+		let operand1 = caps.get(1).unwrap().as_str();
+		let operand2 = caps.get(3).unwrap().as_str();
+		let op = caps.get(2).unwrap().as_str();
 
-	match re2.captures(line) {
-		Some(caps) => {
-			let operand = caps.get(1).unwrap().as_str();
-			let key = caps.get(2).unwrap().as_str();
+		let op1_value = match operand1.parse::<u16>() {
+			Err(_) => get_wire_signal(operand1, map),//recursion,
+			Ok(r) => r,
+		};
 
-			 let value = if let Err(_) = operand.parse::<u16>() {
-				!*map.entry(operand.to_string()).or_insert(0)
-			} else if let Ok(r) = operand.parse::<u16>() {
-				!r
-			} else {
-				0
-			};
+		let op2_value = match operand2.parse::<u16>() {
+			Err(_) => get_wire_signal(operand2, map),//recursion,
+			Ok(r) => r,
+		};
 
-			map.insert(key.to_string(), value);
-			// *map.entry(String::from(key)).or_insert(0)
-		},
-		None => {
-			match re1.captures(line) {
-				Some(caps) => {
-					let f_operand = caps.get(1).unwrap().as_str();
-					let s_operand = caps.get(3).unwrap().as_str();
-					let command = caps.get(2).unwrap().as_str();
-					let key = caps.get(4).unwrap().as_str();
+		// println!("{}", op);
 
-					let operand1 = if let Err(_) = f_operand.parse::<u16>() {
-						map.entry(f_operand.to_string()).or_insert(0);
-						*map.get(f_operand).unwrap()
-					} else if let Ok(r) = f_operand.parse::<u16>() {
-						r
-					} else {
-						0
-					};
-
-					let operand2 = if let Err(_) = s_operand.parse::<u16>() {
-						map.entry(s_operand.to_string()).or_insert(0);
-						*map.get(s_operand).unwrap()
-					} else if let Ok(r) = s_operand.parse::<u16>() {
-						r
-					} else {
-						0
-					};
-
-					*map.entry(String::from(key)).or_insert(0) = match command {
-						"AND" => operand1 & operand2,
-						"OR" => operand1 | operand2,
-						"LSHIFT" => operand1 << operand2,
-						"RSHIFT" => operand1 >> operand2,
-						_ => 0,
-					};
-				},
-				None => (),
-			}
-		},
+		let value = match op {
+			"AND" => op1_value & op2_value,
+			"OR" => op1_value | op2_value,
+			"LSHIFT" => op1_value << op2_value,
+			"RSHIFT" => op1_value >> op2_value,
+			_ => 0,
+		};
+		map.entry(String::from(wire)).and_modify(|e| *e = value.to_string());
+		return value;
 	}
+
+	0
 }
 
 pub fn parsing_file() -> io::Result<()> {
 	let file_name = "../test_day_7.txt";
 	let file = File::open(file_name)?;
 	let reader = BufReader::new(file);
+	let mut map: HashMap<String, String> = HashMap::new();
+	let re_key = Regex::new(r"(.+)\s->\s(\w+)").unwrap();
 
-	let re_op = Regex::new(r"(AND|OR|LSHIFT|RSHIFT|NOT)").unwrap();	
-
-	let mut instruction: HashMap<String, u16> = HashMap::new();
 	for line in reader.lines() {
 		let l = match line {
 			Ok(st) => st,
 			Err(_) => continue,
 		};
 
-		match re_op.captures(&l) {
-			None => {
-				smt1(&l, &mut instruction);
-				continue
+		match re_key.captures(&l) {
+			Some(caps) => {
+				let key = caps.get(2).unwrap().as_str();
+				map.entry(key.to_string()).or_insert(caps.get(1).unwrap().as_str().to_string());
 			},
-			Some(_) => {
-				smt2(&l, &mut instruction);
-			}, 
+			None => continue,
 		}
 	}
 
-	println!("{:?}", instruction);
+	println!("{:?}", get_wire_signal("a", &mut map));
 
 	Ok(())
 }
